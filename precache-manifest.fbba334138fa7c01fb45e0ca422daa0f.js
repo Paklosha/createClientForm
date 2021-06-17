25cacd58b070f88562f1abb0a34606d7df363aee
Paklosha <paklosha@gmail.com>
2021-06-17 12:29:53 +0300

    deploy

---
+++ precache-manifest.fbba334138fa7c01fb45e0ca422daa0f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5dd76bb5096d760f81c5",
+    "url": "/createClientForm/css/app.ab208fe8.css"
+  },
+  {
+    "revision": "e9a43f8583f3910d1d32e3ac3317d510",
+    "url": "/createClientForm/index.html"
+  },
+  {
+    "revision": "5dd76bb5096d760f81c5",
+    "url": "/createClientForm/js/app.8308a23b.js"
+  },
+  {
+    "revision": "74c136d50bc4c6cd8829",
+    "url": "/createClientForm/js/chunk-vendors.20974f0d.js"
+  },
+  {
+    "revision": "4dd2751dfa44c648270334e74aa5aa10",
+    "url": "/createClientForm/manifest.json"
+  }
+]);
